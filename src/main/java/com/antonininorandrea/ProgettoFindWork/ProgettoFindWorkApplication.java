bccff1461841b93fa7afd84fa75c8edc7473205f
Adelio Antonini <adelioantonini@gmail.com>
2021-11-26 15:28:02 +0100

    Initial commit

---
+++ src/main/java/com/antonininorandrea/ProgettoFindWork/ProgettoFindWorkApplication.java
@@ -0,0 +1,13 @@
+package com.antonininorandrea.ProgettoFindWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFindWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFindWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonininorandrea/ProgettoFindWork/ProgettoFindWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonininorandrea.ProgettoFindWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFindWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
