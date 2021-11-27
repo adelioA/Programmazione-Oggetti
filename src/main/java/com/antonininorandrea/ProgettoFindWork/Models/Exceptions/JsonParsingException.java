0ef08bcb4132bbaa2deacbd5c73a495abb7e69c5
Adelio Antonini <adelioantonini@gmail.com>
2021-11-27 03:53:38 +0100

    Added Custom Exceptions

---
+++ src/main/java/com/antonininorandrea/ProgettoFindWork/Models/Exceptions/JsonParsingException.java
@@ -0,0 +1,30 @@
+package com.antonininorandrea.ProgettoFindWork.Models.Exceptions;
+
+public class JsonParsingException extends Exception {
+
+	public JsonParsingException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonParsingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonParsingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonParsingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonParsingException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/antonininorandrea/ProgettoFindWork/Models/Exceptions/Unable2ReachAPI.java
@@ -0,0 +1,29 @@
+package com.antonininorandrea.ProgettoFindWork.Models.Exceptions;
+
+public class Unable2ReachAPI extends Exception {
+
+	public Unable2ReachAPI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Unable2ReachAPI(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Unable2ReachAPI(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Unable2ReachAPI(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Unable2ReachAPI(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
