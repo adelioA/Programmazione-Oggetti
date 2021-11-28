87f5f7854aa27f8f485d9cd4b2a4b0c8c23f3345
Adelio Antonini <adelioantonini@gmail.com>
2021-11-28 16:46:03 +0100

    Added methods to handle FindWork API requests

--- src/main/java/com/antonininorandrea/ProgettoFindWork/Services/FindWorkService.java
+++ src/main/java/com/antonininorandrea/ProgettoFindWork/Services/FindWorkService.java
@@ -21,12 +21,12 @@ public class FindWorkService {
 	// HOST:			https://findwork.dev/api/jobs/?location=london&search=react&sort_by=relevance
 	// API_KEY:			cd8319f3089e09bdef7995a5268db2c9fa5d0356
 	
-	public JSONArray getAPIResult() throws JSONException, Unable2ReachAPI {
+	public JSONArray getAPIResult(String location) throws JSONException, Unable2ReachAPI {
 		OkHttpClient client = new OkHttpClient.Builder()
 				.build();
 		
 		Request request = new Request.Builder()
-				.url("https://findwork.dev/api/jobs/")
+				.url("https://findwork.dev/api/jobs/?location=" + location)
 				.addHeader("Content-Type", "application/json")
 				.addHeader("Authorization", "Token cd8319f3089e09bdef7995a5268db2c9fa5d0356")
 				.get()
@@ -43,6 +43,10 @@ public class FindWorkService {
 		}
 		
 		return apiResult;
+	}
+	
+	public JSONArray getAPIResult() throws JSONException, Unable2ReachAPI {
+		return getAPIResult("");
 	}
 
 	public LinkedList<JobRecord> getAllJobs() throws Unable2ReachAPI, JsonParsingException {
@@ -65,9 +69,44 @@ public class FindWorkService {
 		return jobs;
 	}
 	
-	// TODO
-	public LinkedList<JobRecord> filterByLocations(LinkedList<JobRecord> collection, String... locations) { return collection; }
+	public LinkedList<JobRecord> getAllJobsByLocations(String... locations) throws Unable2ReachAPI, JsonParsingException {
+		LinkedList<JobRecord> jobs = new LinkedList<>();
+		
+		JSONArray records = null;
+		
+		try {
+			
+			for(String location: locations) {
+				records = this.getAPIResult(location);
+				
+				for(int i = 0; i < records.length(); ++i) {
+					JSONObject item = records.getJSONObject(i);
+					item.put("location", location.toUpperCase());
+					jobs.add(JobRecord.fromJSON(item));
+				}
+			}
+			
+		} catch (JSONException e) {
+			throw new JsonParsingException(e.getMessage());
+		}
+		
+		return jobs;
+	}
 	
-	// TODO
-	public LinkedList<JobRecord> filterByKeywords(LinkedList<JobRecord> collection, String... keywords) { return collection; }
+	public LinkedList<JobRecord> filterByKeyword(LinkedList<JobRecord> collection, String keyword) {
+		LinkedList<JobRecord> filteredCollection = new LinkedList<>();
+		
+		// Per ogni record
+		for(int i = 0; i < collection.size(); ++i) {
+			// Prendo la lista di keyword
+			LinkedList<String> recordKeywords = collection.get(i).getKeywords();
+			
+			// Cerco corrispondenza
+			if (recordKeywords.contains(keyword.toLowerCase())) {
+				filteredCollection.add(collection.get(i));		// Se la trovo aggiungo alla lista filtrata
+			}
+		}
+		
+		return filteredCollection;
+	}
 }
