b81b5183c77adec576bdae4b7f49a2185dcf5e70
Adelio Antonini <adelioantonini@gmail.com>
2021-12-07 15:43:58 +0100

    Refactor

--- src/main/java/com/antonininorandrea/ProgettoFindWork/Models/Exceptions/Unable2ReachAPIException.java
+++ src/main/java/com/antonininorandrea/ProgettoFindWork/Models/Exceptions/Unable2ReachAPIException.java
@@ -1,27 +1,27 @@
 package com.antonininorandrea.ProgettoFindWork.Models.Exceptions;
 
-public class Unable2ReachAPI extends Exception {
+public class Unable2ReachAPIException extends Exception {
 
-	public Unable2ReachAPI() {
+	public Unable2ReachAPIException() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Unable2ReachAPI(String message) {
+	public Unable2ReachAPIException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Unable2ReachAPI(Throwable cause) {
+	public Unable2ReachAPIException(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Unable2ReachAPI(String message, Throwable cause) {
+	public Unable2ReachAPIException(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Unable2ReachAPI(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+	public Unable2ReachAPIException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 		// TODO Auto-generated constructor stub
 	}
