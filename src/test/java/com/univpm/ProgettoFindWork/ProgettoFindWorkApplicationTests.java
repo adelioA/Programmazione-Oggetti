ff7736b1fbc8adf73fa86bacca0733fe1fcaaf41
Adelio Antonini <adelioantonini@gmail.com>
2021-12-07 16:07:20 +0100

    Refactor

--- src/test/java/com/univpm/ProgettoFindWork/ProgettoFindWorkApplicationTests.java
+++ src/test/java/com/univpm/ProgettoFindWork/ProgettoFindWorkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.antonininorandrea.ProgettoFindWork;
+package com.univpm.ProgettoFindWork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
